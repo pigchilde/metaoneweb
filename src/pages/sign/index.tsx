abae27340c0a44753fd199ed5e30d57204adefe1
叶秋萍 <552233@nd.com>
2022-04-28 19:50:05 +0800

    样式修改

--- src/pages/sign/index.tsx
+++ src/pages/sign/index.tsx
@@ -1,7 +1,11 @@
 import styles from './index.scss';
 import { useIntl } from 'umi';
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
 const Sign = () => {
   const intl = useIntl();
+  const tabChange = () => {};
   return (
     <div className={styles['sign-page']}>
       <div className={styles['sign-banner']}>
@@ -11,7 +15,19 @@ const Sign = () => {
           })}
         </h3>
       </div>
-      <div className={styles['sign-main']}></div>
+      <div className={styles['sign-main']}>
+        <Tabs defaultActiveKey="1" onChange={tabChange}>
+          <TabPane tab="Tab 1" key="1">
+            Content of Tab Pane 1
+          </TabPane>
+          <TabPane tab="Tab 2" key="2">
+            Content of Tab Pane 2
+          </TabPane>
+          <TabPane tab="Tab 3" key="3">
+            Content of Tab Pane 3
+          </TabPane>
+        </Tabs>
+      </div>
     </div>
   );
 };
