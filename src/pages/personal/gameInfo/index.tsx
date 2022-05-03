171eada990ce9c7684086b99fbbc3aa06eb87aa7
王文清 <998505@nd.com>
2022-05-03 10:57:17 +0800

    【modify】页面样式修改

--- src/pages/personal/gameInfo/index.tsx
+++ src/pages/personal/gameInfo/index.tsx
@@ -1,5 +1,5 @@
 import styles from './index.scss';
-import { useIntl } from 'umi';
+import { useIntl, Link } from 'umi';
 import { Tabs, Table, Tag, Space } from 'antd';
 
 const gameInfo = () => {
@@ -56,113 +56,121 @@ const gameInfo = () => {
   ];
 
   return (
-    <div className={styles['game-info']}>
-      <ul className={styles['nft-info']}>
-        <li className={styles['img']}>
-          <img src="http://dummyimage.com/431x207"></img>
-        </li>
-        <li className={styles['nft-info-con']}>
-          <dl>
-            <dt>NFT Info</dt>
-            <dd>
-              <ul>
-                <li>Chain:BSC</li>
-                <li>Gaming Strategy:click here </li>
-                <li>Website:click here</li>
-                <li>Genre:io</li>
-                <li>Date:2022/1/402:12:16</li>
-              </ul>
-            </dd>
-            <dd className={styles['des']}>
-              Jump into Earth Zero,where you will mine resources,build your
-              NFTtanks and defend your territory against thousands of other
-              rival players.Battle it out in thrilling military conflicts and
-              spectacular locales alongside your allies to conauer the
-              Metaverse.A uniaue mix of strateay and action with stunnina
-              araphics awaits!
-            </dd>
-            <dd className={styles['button']}>
-              <a href="###">
-                {intl.formatMessage({
-                  id: 'PERSONAL_GAMEINFO_PLAY',
-                })}
-              </a>
-            </dd>
-          </dl>
-        </li>
-      </ul>
-      <ul className={styles['game-data']}>
-        <li className="clearfix">
-          <span className={styles['title']}>
-            {intl.formatMessage({
-              id: 'PERSONAL_GAMEINFO_DATA1',
-            })}
-          </span>
-          <span className={styles['data']}>
-            <i className={styles['num']}>12345</i>
-            <i className={styles['name']}>USDT</i>
-            <i className={styles['status']}>↓0.8%</i>
-          </span>
-        </li>
-        <li className="clearfix">
-          <span className={styles['title']}>
-            {intl.formatMessage({
-              id: 'PERSONAL_GAMEINFO_DATA2',
-            })}
-          </span>
-          <span className={styles['data']}>
-            <i className={styles['num']}>4218.45</i>
-            <i className={styles['name']}>h</i>
-          </span>
-        </li>
-        <li className="clearfix">
-          <span className={styles['title']}>
-            {intl.formatMessage({
-              id: 'PERSONAL_GAMEINFO_DATA3',
-            })}
-          </span>
-          <span className={styles['data']}>
-            <i className={styles['num']}>16067</i>
-            <i className={styles['status']}>↓39</i>
-          </span>
-        </li>
-        <li className="clearfix">
-          <span className={styles['title']}>
-            {intl.formatMessage({
-              id: 'PERSONAL_GAMEINFO_DATA4',
-            })}
-          </span>
-          <span className={styles['data']}>
-            <i className={styles['num']}>12011</i>
-            <i className={styles['status']}>↓4.9%</i>
-          </span>
-        </li>
-      </ul>
-      <div className={styles['game-list']}>
-        <Tabs defaultActiveKey="1" onChange={tabClick}>
-          <TabPane
-            tab={intl.formatMessage({
-              id: 'PERSONAL_GAMEINFO_TAB1',
-            })}
-            key="1"
-          >
-            <Table
-              columns={columns}
-              dataSource={data}
-              pagination={{ position: ['bottomCenter'] }}
-            />
-          </TabPane>
-          <TabPane
-            tab={intl.formatMessage({
-              id: 'PERSONAL_GAMEINFO_TAB2',
-            })}
-            key="2"
-          >
-            Content of Tab Pane 2
-          </TabPane>
-        </Tabs>
+    <>
+      <Link to="/" className={styles['back']}>
+        {'< '}
+        {intl.formatMessage({
+          id: 'PERSONAL_GAMEINFO_BACK',
+        })}
+      </Link>
+      <div className={styles['game-info']}>
+        <ul className={styles['nft-info']}>
+          <li className={styles['img']}>
+            <img src="http://dummyimage.com/431x207"></img>
+          </li>
+          <li className={styles['nft-info-con']}>
+            <dl>
+              <dt>NFT Info</dt>
+              <dd>
+                <ul>
+                  <li>Chain:BSC</li>
+                  <li>Gaming Strategy:click here </li>
+                  <li>Website:click here</li>
+                  <li>Genre:io</li>
+                  <li>Date:2022/1/402:12:16</li>
+                </ul>
+              </dd>
+              <dd className={styles['des']}>
+                Jump into Earth Zero,where you will mine resources,build your
+                NFTtanks and defend your territory against thousands of other
+                rival players.Battle it out in thrilling military conflicts and
+                spectacular locales alongside your allies to conauer the
+                Metaverse.A uniaue mix of strateay and action with stunnina
+                araphics awaits!
+              </dd>
+              <dd className={styles['button']}>
+                <a href="###">
+                  {intl.formatMessage({
+                    id: 'PERSONAL_GAMEINFO_PLAY',
+                  })}
+                </a>
+              </dd>
+            </dl>
+          </li>
+        </ul>
+        <ul className={styles['game-data']}>
+          <li className="clearfix">
+            <span className={styles['title']}>
+              {intl.formatMessage({
+                id: 'PERSONAL_GAMEINFO_DATA1',
+              })}
+            </span>
+            <span className={styles['data']}>
+              <i className={styles['num']}>12345</i>
+              <i className={styles['name']}>USDT</i>
+              <i className={styles['status']}>↓0.8%</i>
+            </span>
+          </li>
+          <li className="clearfix">
+            <span className={styles['title']}>
+              {intl.formatMessage({
+                id: 'PERSONAL_GAMEINFO_DATA2',
+              })}
+            </span>
+            <span className={styles['data']}>
+              <i className={styles['num']}>4218.45</i>
+              <i className={styles['name']}>h</i>
+            </span>
+          </li>
+          <li className="clearfix">
+            <span className={styles['title']}>
+              {intl.formatMessage({
+                id: 'PERSONAL_GAMEINFO_DATA3',
+              })}
+            </span>
+            <span className={styles['data']}>
+              <i className={styles['num']}>16067</i>
+              <i className={styles['status']}>↓39</i>
+            </span>
+          </li>
+          <li className="clearfix">
+            <span className={styles['title']}>
+              {intl.formatMessage({
+                id: 'PERSONAL_GAMEINFO_DATA4',
+              })}
+            </span>
+            <span className={styles['data']}>
+              <i className={styles['num']}>12011</i>
+              <i className={styles['status']}>↓4.9%</i>
+            </span>
+          </li>
+        </ul>
+        <div className={styles['game-list']}>
+          <Tabs defaultActiveKey="1" onChange={tabClick}>
+            <TabPane
+              tab={intl.formatMessage({
+                id: 'PERSONAL_GAMEINFO_TAB1',
+              })}
+              key="1"
+            >
+              <Table
+                columns={columns}
+                dataSource={data}
+                pagination={{ position: ['bottomCenter'] }}
+              />
+            </TabPane>
+            <TabPane
+              tab={intl.formatMessage({
+                id: 'PERSONAL_GAMEINFO_TAB2',
+              })}
+              key="2"
+            >
+              Content of Tab Pane 2
+            </TabPane>
+          </Tabs>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default gameInfo;
--- src/pages/personal/setting/index.tsx
+++ src/pages/personal/setting/index.tsx
@@ -1,5 +1,5 @@
 import styles from './index.scss';
-import { useIntl } from 'umi';
+import { useIntl, Link } from 'umi';
 import { Form, Input, Button, Avatar, Row, Col } from 'antd';
 
 const setting = () => {
@@ -14,58 +14,102 @@ const setting = () => {
   };
 
   return (
-    <div className={styles['setting']}>
-      <Form
-        name="basic"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-        layout="vertical"
-      >
-        <Row className={styles['setting-con']}>
-          <Col span={5}>
-            <Form.Item label="" name="" className="avatar">
-              <Avatar size={163} src="http://dummyimage.com/163x163" />
-              <p>Edit Your Logo</p>
-            </Form.Item>
-          </Col>
-          <Col span={19} className={styles['setting-con-item']}>
-            <Form.Item name="name" label="Guild Name">
-              <Input />
-            </Form.Item>
-            <Form.Item name="name" label="Category">
-              <Input />
-            </Form.Item>
-            <Form.Item name="name" label="Telegram ID">
-              <Input placeholder="Enter Your Tg" />
-            </Form.Item>
-            <Form.Item name="name" label="Contact">
-              <Input placeholder="Enter Your Contact " />
-            </Form.Item>
-            <Form.Item name="name" label="Guild Size">
-              <Input placeholder="Enter Guild Size" />
-            </Form.Item>
-            <Form.Item name="name" label="Country">
-              <Input placeholder="Enter Your Country" />
-            </Form.Item>
-            <Form.Item
-              name="name"
-              label="Guild Size"
-              /* className={styles['setting-textArea']} */
-              className="setting-textarea"
-            >
-              <TextArea rows={4} placeholder="Enter Your Description" />
-            </Form.Item>
-            <Form.Item name="name" label="">
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>
-    </div>
+    <>
+      <Link to="/" className={styles['back']}>
+        {'< '}
+        {intl.formatMessage({
+          id: 'PERSONAL_GUILD_BACK',
+        })}
+      </Link>
+      <div className={styles['setting']}>
+        <Form
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          layout="vertical"
+        >
+          <Row className={styles['setting-con']}>
+            <Col span={5}>
+              <Form.Item label="" name="" className="avatar">
+                <Avatar size={163} src="http://dummyimage.com/163x163" />
+                <p>
+                  {intl.formatMessage({
+                    id: 'PERSONAL_SETTING_EDIT_AVATAR',
+                  })}
+                </p>
+              </Form.Item>
+            </Col>
+            <Col span={19} className={styles['setting-con-item']}>
+              <Form.Item
+                name="name"
+                label={intl.formatMessage({
+                  id: 'PERSONAL_SETTING_EDIT_GUILD_NAME',
+                })}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                name="name"
+                label={intl.formatMessage({
+                  id: 'PERSONAL_SETTING_EDIT_GUILD_CATEGORY',
+                })}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                name="name"
+                label={intl.formatMessage({
+                  id: 'PERSONAL_SETTING_EDIT_GUILD_TELEGRAMID',
+                })}
+              >
+                <Input placeholder="Enter Your Tg" />
+              </Form.Item>
+              <Form.Item
+                name="name"
+                label={intl.formatMessage({
+                  id: 'PERSONAL_SETTING_EDIT_GUILD_CONTACT',
+                })}
+              >
+                <Input placeholder="Enter Your Contact " />
+              </Form.Item>
+              <Form.Item
+                name="name"
+                label={intl.formatMessage({
+                  id: 'PERSONAL_SETTING_EDIT_GUILD_SIZE',
+                })}
+              >
+                <Input placeholder="Enter Guild Size" />
+              </Form.Item>
+              <Form.Item
+                name="name"
+                label={intl.formatMessage({
+                  id: 'PERSONAL_SETTING_EDIT_GUILD_COUNTRY',
+                })}
+              >
+                <Input placeholder="Enter Your Country" />
+              </Form.Item>
+              <Form.Item
+                name="name"
+                label={intl.formatMessage({
+                  id: 'PERSONAL_SETTING_EDIT_GUILD_SIZE',
+                })}
+                /* className={styles['setting-textArea']} */
+                className="setting-textarea"
+              >
+                <TextArea rows={4} placeholder="Enter Your Description" />
+              </Form.Item>
+              <Form.Item name="name" label="">
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </div>
+    </>
   );
 };
 export default setting;
