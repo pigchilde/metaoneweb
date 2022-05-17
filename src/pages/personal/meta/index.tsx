650e70a0bd2656dca99e5d26bd04ea10aa89792f
叶秋萍 <552233@nd.com>
2022-05-17 16:56:39 +0800

    钱包

---
+++ src/pages/personal/meta/index.tsx
@@ -0,0 +1,10 @@
+import MetaMask from '@/components/MetaMask';
+import styles from './index.scss';
+const Meta = () => {
+  return (
+    <div className={styles['meta']}>
+      <MetaMask />
+    </div>
+  );
+};
+export default Meta;
