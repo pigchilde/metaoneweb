7f18d912383e1468b8541af8fd6979ebc23115fd
叶秋萍 <552233@nd.com>
2022-05-26 04:05:14 -0300

    添加文件加载

--- .umirc.ts
+++ .umirc.ts
@@ -8,6 +8,17 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  chainWebpack: (config) => {
+    config.module
+      .rule('mp4')
+      .test(/\.(mp4|zip)(\?.*)?$/)
+      .use('file-loader')
+      .loader(require.resolve('file-loader'))
+      .options({
+        name: 'static/[name].[hash:8].[ext]',
+        esModule: false,
+      });
+  },
   fastRefresh: {},
   hash: true,
   proxy: {
