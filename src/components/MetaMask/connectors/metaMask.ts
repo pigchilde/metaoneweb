3b222794ba07575dd69dc448e878b03c71f52c11
叶秋萍 <552233@nd.com>
2022-06-06 18:47:21 +0800

    修改

--- src/components/MetaMask/connectors/metaMask.ts
+++ src/components/MetaMask/connectors/metaMask.ts
@@ -5,7 +5,7 @@ import { initializeConnector } from '@web3-react/core';
 console.log(initializeConnector, 3536868);
 export const [metaMask, hooks] = initializeConnector<MetaMask>(
   (actions: Actions) => {
-    console.log(actions, 5658686);
+    console.log(actions, 565866);
     return new MetaMask(actions);
   },
 );
